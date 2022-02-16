e933a289caac3e68e7b63a638dea08ea003010d3
Andrea Brugaletta <andreabrugaletta@yahoo.it>
2022-02-16 16:55:08 +0100

    homepage done

--- index.php
+++ index.php
@@ -73,29 +73,52 @@ if (!isset($_SESSION['user_data'])) {
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3">
-                    <div class="p-5" style="width: 21rem;">
-                        <img style="width: 250px; height: 250px;" src="assets/img/chalkboard-teacher-solid.svg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Le mie riunioni</h5>
-                            <p class="card-text">Gestici le tue riunioni. Organizza, modifica o cancella una riunione.</p>
-                            <a href="./frontend/le%20mie%20riunioni.php" class="btn btn-primary">Vai alle riunioni</a>
-                        </div>
-                    </div>
+                <?php
+                if ($_SESSION['user_data']['data_autorizzazione'] != null) {
+                    showAuthUserCards();
+                }
+                if ($_SESSION['user_data']['ruolo'] === 'direttore') {
+                    showAdminCards();
+                }
+                ?>
+    </main>
+    <?php require './common/footer.html'; ?>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="./js/datatables-simple-demo.js"></script>
+</body>
+</html>
+
+<?php
+function showAuthUserCards()
+{
+    echo '<div class="col-sm-3">
+            <div class="p-5" style="width: 21rem;">
+                <img style="width: 250px; height: 250px;" src="assets/img/chalkboard-teacher-solid.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Le mie riunioni</h5>
+                    <p class="card-text">Gestici le tue riunioni. Organizza, modifica o cancella una riunione.</p>
+                    <a href="./frontend/le%20mie%20riunioni.php" class="btn btn-primary">Vai alle riunioni</a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-4">
-                    <div class="p-5" style="width: 21rem;">
-                        <img style="width: 250px; height: 250px" src="assets/img/users-solid.svg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Gestisci utenti</h5>
-                            <p class="card-text">Gestisci i dati utente del personale dell'azienda.</p>
-                            <a href="./frontend/gestione%20utenti.php" class="btn btn-primary">Vai agli utenti</a>
-                        </div>
+        </div>';
+}
+
+function showAdminCards()
+{
+    echo '<div class="row">
+            <div class="col-4">
+                <div class="p-5" style="width: 21rem;">
+                    <img style="width: 250px; height: 250px" src="assets/img/users-solid.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Gestisci utenti</h5>
+                        <p class="card-text">Gestisci i dati utente del personale dell\'azienda.</p>
+                        <a href="./frontend/gestione%20utenti.php" class="btn btn-primary">Vai agli utenti</a>
                     </div>
                 </div>
-                <div class="col-sm-4">
+            </div>';
+    echo '<div class="col-sm-4">
                     <div class="p-5" style="width: 21rem;">
                         <img style="height: 250px; width: 250px" src="assets/img/building-regular.svg" class="card-img-top" alt="...">
                         <div class="card-body">
@@ -105,12 +128,8 @@ if (!isset($_SESSION['user_data'])) {
                         </div>
                     </div>
                 </div>
-            </div>
-    </main>
-    <?php require './common/footer.html'; ?>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-<script src="./js/datatables-simple-demo.js"></script>
-</body>
-</html>
+            </div>';
+}
+
+
+?>
