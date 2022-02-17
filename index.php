def9afc684979d0171d40146c5f9c154296a0cef
Andrea Brugaletta <andreabrugaletta@yahoo.it>
2022-02-17 15:15:16 +0100

    project done

--- index.php
+++ index.php
@@ -33,10 +33,6 @@ if (!isset($_SESSION['user_data'])) {
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <a class="navbar-brand ps-3" href="index.php">Meetup Planner</a>
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-        <div class="input-group">
-            <input class="form-control" type="text" placeholder="Cerca..." aria-label="Cerca..." aria-describedby="btnNavbarSearch" />
-            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-        </div>
     </form>
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
