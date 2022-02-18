b074ae72f3c23d99cc5c57b53a4dbf44024bbd8d
Andrea Brugaletta <andreabrugaletta@yahoo.it>
2022-02-18 09:43:32 +0100

    prende nome sala in base al dip

--- js/nomi_sale.js
+++ js/nomi_sale.js
@@ -1,41 +1,38 @@
-window.addEventListener('load', () => {
-  console.log('window load')
-  const roomsSelect = document.getElementById('sala_select')
-  const formChecks = document.getElementsByClassName('form-check-input')
-  const formChecksArr = [].slice.call(formChecks)
-  let department = ''
+const roomsSelect = document.getElementById('sala_select')
+const formChecks = document.getElementsByClassName('form-check-input')
+const formChecksArr = [].slice.call(formChecks)
+let department = ''
 
-  formChecksArr.forEach((checkInput) => {
-    checkInput.addEventListener('click', () => {
-      roomsSelect.textContent = ''
-      department = checkInput.value
+formChecksArr.forEach((checkInput) => {
+  checkInput.addEventListener('click', () => {
+    roomsSelect.textContent = ''
+    department = checkInput.value
 
-      const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-      request.onload = () => {
-        let responseObject = null
-        try {
-          responseObject = JSON.parse(request.responseText)
-        } catch (e) {
-          console.error('Could not parse JSON!')
-        }
-
-        responseObject.rooms.forEach((room) => {
-          let option = document.createElement('option') //<option></option>
-          option.setAttribute('value', room.nome) //<option value="nome"></option>
-          let text = document.createTextNode(room.nome) //nome
-          option.appendChild(text) //<option value="nome">nome</option>
-          roomsSelect.appendChild(option)
-        })
+    request.onload = () => {
+      let responseObject = null
+      try {
+        responseObject = JSON.parse(request.responseText)
+      } catch (e) {
+        console.error('Could not parse JSON!')
       }
 
-      const requestData = `dp=${department}`
-      request.open('post', '../backend/lista_nomisale_back.php')
-      request.setRequestHeader(
-        'Content-type',
-        'application/x-www-form-urlencoded',
-      )
-      request.send(requestData)
-    })
+      responseObject.rooms.forEach((room) => {
+        let option = document.createElement('option') //<option></option>
+        option.setAttribute('value', room.nome) //<option value="nome"></option>
+        let text = document.createTextNode(room.nome) //nome
+        option.appendChild(text) //<option value="nome">nome</option>
+        roomsSelect.appendChild(option)
+      })
+    }
+
+    const requestData = `dp=${department}`
+    request.open('post', '../backend/lista_nomisale_back.php')
+    request.setRequestHeader(
+      'Content-type',
+      'application/x-www-form-urlencoded',
+    )
+    request.send(requestData)
   })
 })
